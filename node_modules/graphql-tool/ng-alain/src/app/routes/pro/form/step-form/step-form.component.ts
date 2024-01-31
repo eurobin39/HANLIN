d797c9908f59265951ebaecb7496db135a884d0c
eurobae <dkfvksmreo12@naver.com>
2024-01-31 15:39:53 +0900

    24.01.31(setting)_GQL

---
+++ node_modules/graphql-tool/ng-alain/src/app/routes/pro/form/step-form/step-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { TransferService } from './transfer.service';
+
+@Component({
+    selector: 'app-step-form',
+    templateUrl: './step-form.component.html',
+    styleUrls: [ './step-form.component.less' ],
+    providers: [ TransferService ],
+    encapsulation: ViewEncapsulation.None
+})
+export class StepFormComponent implements AfterViewInit {
+
+    constructor(public item: TransferService) {}
+
+    ngAfterViewInit() {
+        console.log('item', this.item);
+    }
+}
